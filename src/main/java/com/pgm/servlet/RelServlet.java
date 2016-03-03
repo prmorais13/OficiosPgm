6ffd3cd071876dd4964a8a0204d0380d6e2841cb
paulo <pauloroberto@natal.rn.gov.br>
2016-03-02 21:18:01 -0300

    Nova servlet

--- src/main/java/com/pgm/servlet/RelServlet.java
+++ src/main/java/com/pgm/servlet/RelServlet.java
@@ -1,8 +1,6 @@
 package com.pgm.servlet;
 
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,6 +11,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.pgm.util.report.RelatorioUtil;
 
@@ -23,8 +22,31 @@ public class RelServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession session = request.getSession(true);
+		
+		Date dataInicial = (Date) session.getAttribute("dataInicio");
+		Date dataFinal = (Date) session.getAttribute("dataFinal");
+		
+		Map<String, Object> parametros = new HashMap<>();
+		parametros.put("data_inicio", dataInicial);
+		parametros.put("data_final", dataFinal);
+		
+		String arqJasper = getServletContext().getRealPath(
+				"/WEB-INF/relatorios/relatorio_oficios.jasper");
+		
+		byte[] bytes = RelatorioUtil.criarRelatorio(arqJasper, parametros);
+		
+		if (bytes != null && bytes.length > 0) {
+			response.setContentType("application/pdf");
+			response.setContentLength(bytes.length);
+			ServletOutputStream ouputStream = response.getOutputStream();
+			ouputStream.write(bytes, 0, bytes.length);
+			ouputStream.flush();
+			ouputStream.close();
+		}
 						
-		String dataInicio = request.getParameter("dataInicio");
+		/*String dataInicio = request.getParameter("dataInicio");
 		String dataFinal = request.getParameter("dataFinal");
 		
 		System.out.println(dataInicio);
@@ -57,11 +79,9 @@ public class RelServlet extends HttpServlet {
 			
 		} catch (ParseException e) {
 			e.printStackTrace();
-		}
+		}*/
 		
-		
-		
-		
+
 		
 	}
 
